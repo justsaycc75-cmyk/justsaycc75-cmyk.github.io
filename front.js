263eef137852ef3f2b2dd593e423621806aaf077
justsaycc75-cmyk <justsaycc75@yahoo.com.au>
2026-09-23 15:59:29 +1000

    Point current news shortcut to Google News Australia

--- front.js
+++ front.js
@@ -137,7 +137,7 @@
   rail.innerHTML=`
     <a href="music.html" class="front-photo-card"><img src="assets/img/cassette.webp" alt="Music"><span>Music</span><em>Daily clip + archive</em></a>
 
-    <a href="https://www.reuters.com/world/" class="front-photo-card image-only-card news-rail-card" aria-label="Open latest world news" target="_blank" rel="noopener">
+    <a href="https://news.google.com/home?hl=en-AU&gl=AU&ceid=AU:en" class="front-photo-card image-only-card news-rail-card" aria-label="Open latest world news" target="_blank" rel="noopener">
       <img src="News.png?v=20260921" alt="World news and latest headlines">
     </a>
 
